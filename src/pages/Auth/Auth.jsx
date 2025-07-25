edad9cb16b87aae17ff230db85503d4df17f7c1e
unknown <vinaykushwah890@gmail.com>
2025-07-25 14:45:03 +0530

    feat: add login and register pages with basic functionality

---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Login from "../../components/Login";
+import Register from "../../components/Register";
+const Auth = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#08080a] px-4">
+      <div className="max-w-md w-full space-y-8">
+        {/* Logo and Title */}
+        <div className="text-center">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <div className="bg-[#3c73ec] w-13  h-13 flex justify-center items-center p-2 rounded-md">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-bug-icon lucide-bug text-white"
+              >
+                <path d="m8 2 1.88 1.88" />
+                <path d="M14.12 3.88 16 2" />
+                <path d="M9 7.13v-1a3.003 3.003 0 1 1 6 0v1" />
+                <path d="M12 20c-3.3 0-6-2.7-6-6v-3a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v3c0 3.3-2.7 6-6 6" />
+                <path d="M12 20v-9" />
+                <path d="M6.53 9C4.6 8.8 3 7.1 3 5" />
+                <path d="M6 13H2" />
+                <path d="M3 21c0-2.1 1.7-3.9 3.8-4" />
+                <path d="M20.97 5c0 2.1-1.6 3.8-3.5 4" />
+                <path d="M22 13h-4" />
+                <path d="M17.2 17c2.1.1 3.8 1.9 3.8 4" />
+              </svg>
+            </div>
+            <h1 className="text-3xl font-bold text-[#3c73ec]">BugLine</h1>
+          </div>
+          <p className="text-gray-400 mt-2">
+            {isLogin ? "Sign in to your account" : "Create a new account"}
+          </p>
+        </div>
+
+        {/* Form */}
+        {isLogin ? (
+          <Login onClick={() => setIsLogin(false)} />
+        ) : (
+          <Register onClick={() => setIsLogin(true)} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
