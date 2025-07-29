3bf96aa77325b71a871b6ffbdde825126c7949e2
unknown <vinaykushwah890@gmail.com>
2025-07-30 01:02:56 +0530

    feat: create Profile Management UI

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import AddEmployee from "./pages/Employee/AddEmployee";
+import Profile from "./pages/Profile";
 const App = () => {
   return (
     <BrowserRouter>
@@ -12,6 +13,8 @@ const App = () => {
         <Route path="/auth" element={<Auth />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/addemployee" element={<AddEmployee />} />
+        <Route path="/profile" element={<Profile />} />
+
         {/* Add more routes here as needed */}
       </Routes>
     </BrowserRouter>
